3ab3bbe1e130574732d9eaf8f34fb35a7ad8005d
hessm8 <hessmares@gmail.com>
2022-04-14 14:49:41 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gamescore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
