979cd2c500131a9002a27a26a3e2c0d69433917c
hessm8 <hessmares@gmail.com>
2022-04-15 15:06:55 +0700

    Add generic repository

---
+++ Data/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Gamescore.Data.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity?> Get(int id);
+        Task<TEntity> Create(TEntity item);
+        void Update(TEntity item);
+        void Delete(TEntity item);
+    }
+}
---
+++ Data/Repositories/Repository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gamescore.Data.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly ApplicationDbContext context;
+        protected DbSet<TEntity> Entities => context.Set<TEntity>();
+
+        public Repository(ApplicationDbContext appDbContext)
+        {
+            context = appDbContext;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await Entities.ToListAsync();
+        }
+
+        public async Task<TEntity?> Get(int id)
+        {
+            return await Entities.FindAsync(id);
+        }
+
+        public async Task<TEntity> Create(TEntity entity)
+        {
+            var entry = await Entities.AddAsync(entity);
+            return entry.Entity;
+        }
+
+        public void Update(TEntity entity)
+        {
+            Entities.Update(entity);
+        }
+
+        public void Delete(TEntity item)
+        {
+            Entities.Remove(item);
+        }
+    }
+}
