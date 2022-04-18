4196dc46b903d7b88b31bebf73ed344e8aa40e54
hessm8 <hessmares@gmail.com>
2022-04-18 15:55:17 +0700

    Add UnitOfWork

---
+++ Data/Repositories/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Gamescore.Entities;
+
+namespace Gamescore.Data.Repositories
+{
+    public interface IUnitOfWork
+    {
+        public IRepository<Game> Games { get; }
+        public IRepository<Session> Sessions { get; }
+        public IRepository<UserProfile> Profiles { get; }
+        public Task<int> Save();
+    }
+}
---
+++ Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Gamescore.Entities;
+
+namespace Gamescore.Data.Repositories
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly ApplicationDbContext context;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            this.context = context;
+            Games = new Repository<Game>(context);
+            Sessions = new Repository<Session>(context);
+            Profiles = new Repository<UserProfile>(context);
+        }
+        public IRepository<Game> Games { get; }
+        public IRepository<Session> Sessions { get; }
+        public IRepository<UserProfile> Profiles { get; }
+
+        public async Task<int> Save() => await context.SaveChangesAsync();
+
+        private bool disposed = false;
+        public virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing) context.Dispose();
+                disposed = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
