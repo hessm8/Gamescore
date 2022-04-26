e9c178ac052c9a2fea584d53814562b20447024b
hessm8 <hessmares@gmail.com>
2022-04-26 14:07:47 +0700

    Add the list of games (markup)
    Uses placeholder games
    Add rating hover animation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
+using Gamescore.Entities;
+
 namespace Gamescore.Controllers
 {
     public class HomeController : Controller
@@ -18,9 +20,34 @@ namespace Gamescore.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Games()
+        {           
+            return View(GenerateGames());
+        }
+
+        // Dummy games
+        private IEnumerable<Game> GenerateGames()
         {
-            return View();
+            var games = new List<Game>();
+
+            for (int i = 0; i < 15; i++)
+            {
+                games.Add(new Game()
+                {
+                    Alias = "game" + i,
+                    Name = "The Game of Chess and Something Else " + i,
+                    NameLocalized = "Какая-то Игра и Что-то Еще " + i,
+                    AgeMin = i,
+                    ReleaseDate = DateTime.Now,
+                    PlayersMin = 1,
+                    PlayersMax = i,
+                    DurationMin = 10,
+                    DurationMax = 15 + i,
+
+                });
+            }
+
+            return games;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
