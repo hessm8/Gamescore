fbb33b8cd9947376fa4853abbc50828aa2db3e84
hessm8 <hessmares@gmail.com>
2022-06-17 15:18:02 +0700

    Home page :)

--- Gamescore.Web/Controllers/HomeController.cs
+++ Gamescore.Web/Controllers/HomeController.cs
@@ -4,21 +4,25 @@ using System.Diagnostics;
 
 using Gamescore.Domain.Entities;
 using Gamescore.DAL;
+using Gamescore.BLL.Services;
 
 namespace Gamescore.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> logger;
+        private readonly IGameService gameService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IGameService gameService)
         {
             this.logger = logger;
+            this.gameService = gameService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var result = await gameService.GetAll();
+            return View(result.Take(10));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
